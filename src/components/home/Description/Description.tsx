24624d6786508784dc95e23e79c2be8c20c41005
Johan Viancha <vianchajohan@gmail.com>
2024-06-17 16:02:00 -0500

    next image

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./Description.module.sass";
 
 export function Description() {
-  return <section>
-    <h1>Descripción</h1>
-  </section>;
+  return (
+    <section className={styles.Description}>
+      <Image
+        src="/images/description.jpeg"
+        alt="products marketplace"
+        width={500}
+        height={300}
+      />
+      <div className={styles.Description__text}>
+        <h2>Description</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente a
+          facilis quod commodi quisquam maxime distinctio expedita rem, natus
+          nisi!
+        </p>
+      </div>
+    </section>
+  );
 }
